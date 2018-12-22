614ddb14e915aec26e7465dd38ce6d3d3e502c46
Nsereko Nicodemus <niconsereko12@gmail.com>
2018-12-22 13:57:05 +0300

    [delivers #162823579] Add api endpoint and tests for create red-flag

---
+++ application/routes/route.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+import datetime
+from application.models.model import Incident
+
+app = Flask(__name__)
+
+statuses =('draft', 'rejected', 'under investigation', 'resolved')
+ttype = ('red-flag', 'intervention')
+all_redflags= [
+    #   {
+    #       "id": 0,
+    #       "createdOn": datetime.datetime.utcnow(),
+    #       "createdBy" : 1,
+    #       "type" : 1,
+    #       "location" : "lat 0.00333 long 1.3456",
+    #       "status" : "Draft",
+    #       "comment" : "This is my comment."
+    #   }
+]
+
+@app.route('/')
+def home():
+    return jsonify({'message':"Welcome to iReporter"}),200
+
+
+
+@app.route('/api/v1/red-flags', methods=['POST'])
+def create_redflag():
+    data_request = request.get_json()
+    id = len(all_redflags)+1
+    createdOn = data_request.get('createdOn')
+    createdBy = data_request.get('createdBy')
+    incident_type = data_request.get('incident_type')
+    location = data_request.get('location')
+    incident_status = data_request.get('incident_status')
+    comment = data_request.get('comment')
+
+    myObject = Incident(**{'id':id,'createdOn':createdOn, 'createdBy':createdBy, 'incident_type':incident_type, 'location':location, 'incident_status':incident_status, 'comment':comment})
+    if(createdBy == None or myObject.incident_type == '' or myObject.incident_status == ''):
+        return jsonify({'error':'Please add missing fields'}),400
+    if incident_status not in statuses:
+        return jsonify({'error':'Incident status should either be draft, or rejected, or under investigation, or resolved'}), 400
+    if incident_type not in ttype:
+        return jsonify({'error':'Incident type should either be red-flag or intervention'}), 400
+    if (len(myObject.comment)>30):
+        return jsonify({'error':'Do not exceed 30 characters'}),400
+    
+    all_redflags.append(myObject.getRecord())
+    return jsonify({
+                'status':201,
+                "data": [{
+                    'id': myObject.getRecord()['id'],
+                    "message": "Created red-flag record"
+                }]    
+            }), 201
+
---
+++ tests/test_application.py
@@ -0,0 +1,28 @@
+import unittest
+import json
+from application.routes.route import app
+
+class TestApplication(unittest.TestCase):
+    def setUp(self):
+        self.tester = app.test_client()
+
+    def test_home(self):
+        res = self.tester.get('/')
+        self.assertTrue(res.status_code == 200)
+        self.assertTrue("Welcome to iReporter" in str(res.data))
+
+
+    def test_create_red_flag(self):
+        res = self.tester.post('/api/v1/red-flags', json={
+            "createdOn": '14/10/2018',
+            "createdBy": 1,
+            "id":1,
+            "incident_type": "red-flag",
+            "location": "lat 0.00333 long 1.3456",
+            "incident_status": "draft",
+            "comment": "This is my comment."
+        })
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 201)
+        self.assertTrue('Created red-flag record' in str(res.data))
+
