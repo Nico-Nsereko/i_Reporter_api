098c32144927bb99b530fc011d422aa0c8635c8a
Nsereko Nicodemus <niconsereko12@gmail.com>
2018-12-22 14:32:10 +0300

    [delivers #162823585] Add code for edit specific redflag record

---
+++ application/routes/route.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+import datetime
+from application.models.model import Incident
+
+app = Flask(__name__)
+
+statuses =('draft', 'rejected', 'under investigation', 'resolved')
+ttype = ('red-flag', 'intervention')
+all_redflags= [
+    #   {
+    #       "id": 0,
+    #       "createdOn": datetime.datetime.utcnow(),
+    #       "createdBy" : 1,
+    #       "type" : 1,
+    #       "location" : "lat 0.00333 long 1.3456",
+    #       "status" : "Draft",
+    #       "comment" : "This is my comment."
+    #   }
+]
+
+@app.route('/')
+def home():
+    return jsonify({'message':"Welcome to iReporter"}),200
+
+
+
+@app.route('/api/v1/red-flags', methods=['POST'])
+def create_redflag():
+    data_request = request.get_json()
+    id = len(all_redflags)+1
+    createdOn = data_request.get('createdOn')
+    createdBy = data_request.get('createdBy')
+    incident_type = data_request.get('incident_type')
+    location = data_request.get('location')
+    incident_status = data_request.get('incident_status')
+    comment = data_request.get('comment')
+
+    myObject = Incident(**{'id':id,'createdOn':createdOn, 'createdBy':createdBy, 'incident_type':incident_type, 'location':location, 'incident_status':incident_status, 'comment':comment})
+    if(createdBy == None or myObject.incident_type == '' or myObject.incident_status == ''):
+        return jsonify({'error':'Please add missing fields'}),400
+    if incident_status not in statuses:
+        return jsonify({'error':'Incident status should either be draft, or rejected, or under investigation, or resolved'}), 400
+    if incident_type not in ttype:
+        return jsonify({'error':'Incident type should either be red-flag or intervention'}), 400
+    if (len(myObject.comment)>30):
+        return jsonify({'error':'Do not exceed 30 characters'}),400
+    
+    all_redflags.append(myObject.getRecord())
+    return jsonify({
+                'status':201,
+                "data": [{
+                    'id': myObject.getRecord()['id'],
+                    "message": "Created red-flag record"
+                }]    
+            }), 201
+
+@app.route('/api/v1/red-flags', methods=['GET'])
+def get_redflags():
+    if len(all_redflags)>0:
+        return jsonify({
+            'status': 200,
+            "data": [all_redflags] }), 200
+    return jsonify({"message": "No red flags detected"}), 400
+
+@app.route('/api/v1/red-flags/<int:red_flag_id>', methods=['GET'])
+def get_redflag(red_flag_id):
+    specific = []
+    for redflag in all_redflags:
+        if redflag['id'] == red_flag_id:
+            specific.append(redflag['id'])
+            return jsonify({
+                'status':redflag['id'],
+                'data': [redflag]}),200
+    return jsonify({'error': 'Red-flag not available'}),404
+
+@app.route('/api/v1/red-flags/<int:red_flag_id>/location', methods=['PATCH'])
+def edit_location(red_flag_id):
+    for redflag in all_redflags:
+        if redflag['id'] == red_flag_id:
+            redflag['location'] = request.get_json().get('location')
+            if(len(redflag['location'])==0):
+                return jsonify({'error':"Location can't be empty"}),404
+            return jsonify({
+                'status':200,
+                'data': [{
+                    'id':redflag['id'],
+                    'message':"Updated red-flag record's location"
+                }]}),200
+    return jsonify({'error': 'Red-flag not available'}),400 
+
+@app.route('/api/v1/red-flags/<int:red_flag_id>/comment', methods=['PATCH'])
+def edit_comment(red_flag_id):
+    for redflag in all_redflags:
+        if redflag['id'] == red_flag_id:
+            redflag['comment'] = request.get_json().get('comment')
+            if(len(redflag['comment'])==0):
+                return jsonify({'error':"Comment can't be empty"}),404
+            return jsonify({
+                'status':200,
+                'data': [{
+                    'id':redflag['id'],
+                    'message':"Updated red-flag record's comment"
+                }]}),200
+    return jsonify({'error': 'Red-flag not available'}),400 
+
---
+++ tests/test_application.py
@@ -0,0 +1,87 @@
+import unittest
+import json
+from application.routes.route import app
+
+class TestApplication(unittest.TestCase):
+    def setUp(self):
+        self.tester = app.test_client()
+
+    def test_home(self):
+        res = self.tester.get('/')
+        self.assertTrue(res.status_code == 200)
+        self.assertTrue("Welcome to iReporter" in str(res.data))
+
+
+    def test_create_red_flag(self):
+        res = self.tester.post('/api/v1/red-flags', json={
+            "createdOn": '14/10/2018',
+            "createdBy": 1,
+            "id":1,
+            "incident_type": "red-flag",
+            "location": "lat 0.00333 long 1.3456",
+            "incident_status": "draft",
+            "comment": "This is my comment."
+        })
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 201)
+        self.assertTrue('Created red-flag record' in str(res.data))
+
+    def test_getredflags(self):
+        res = self.tester.get('/api/v1/red-flags')
+        self.assertIn("data", str(res.data))
+        self.assertEqual(res.status_code, 200)
+
+    def test_getredflag(self):
+        res = self.tester.get('/api/v1/red-flags/1')
+        self.assertEqual(res.status_code, 200)
+
+    def test_redflag_not_found(self):
+        res = self.tester.get('/api/v1/red-flags/4')
+        self.assertTrue(res.status_code == 404)
+        self.assertTrue('Red-flag not available' in str(res.data))
+
+    def test_edit_location(self):
+        res = self.tester.patch('/api/v1/red-flags/1/location', json=dict(
+            location= "lat 0.44 long 1.23444"
+        ))
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("Updated red", str(res.data))
+        self.assertIn("location", str(res.data))
+
+    def test_edit_comment(self):
+        res = self.tester.patch('/api/v1/red-flags/1/comment', json=(
+            {
+                'comment':'This is the updated comment'
+            }
+        ))
+        data = json.loads(res.data)
+        self.assertTrue(res.status_code == 200)
+        self.assertIn("Updated red", str(res.data))
+        self.assertIn("comment", str(res.data))
+
+    def test_not_edited_comment(self):
+        res = self.tester.patch('/api/v1/red-flags/1/comment', json={
+            'comment': ''
+        })
+        self.assertTrue(res.status_code == 404)
+        self.assertIn("Comment can", str(res.data))
+
+        res = self.tester.patch('/api/v1/red-flags/4/comment', json={
+            'comment': 'This is a new comment'
+        })
+        self.assertTrue(res.status_code == 400)
+        self.assertTrue("Red-flag not available" in str(res.data))
+
+    def test_not_edited_location(self):
+        res = self.tester.patch('/api/v1/red-flags/4/location', json={
+            'location': "lat 0.44 long 1.23444"
+        })
+        self.assertTrue(res.status_code == 400)
+        self.assertIn("Red-flag not",str(res.data))
+
+        res = self.tester.patch('/api/v1/red-flags/1/location', json={
+            'location': ""
+        })
+        self.assertTrue(res.status_code == 404)
+        self.assertIn("t be empty",str(res.data))
